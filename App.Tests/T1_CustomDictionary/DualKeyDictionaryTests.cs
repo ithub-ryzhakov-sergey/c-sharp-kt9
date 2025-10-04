d2a6a7b61d63aaad1d28a9af1184ecb72ea33200
user1 <user1@DESKTOP-D0T1IQ4>
2025-10-04 11:41:56 +0300

    14141

--- App.Tests/T1_CustomDictionary/DualKeyDictionaryTests.cs
+++ App.Tests/T1_CustomDictionary/DualKeyDictionaryTests.cs
@@ -1,60 +1,65 @@
-namespace App.Tests.T1_CustomDictionary;
+using System;
+using NUnit.Framework;
+using App.Topics.Generics.T1_CustomDictionary; // Важная директива using
 
-[TestFixture]
-public class DualKeyDictionaryTests
+namespace App.Tests_TL_CustomDictionary
 {
-    [Test]
-    public void SetAndGet_ByIntKey_Works_For_ReferenceType()
+    [TestFixture]
+    public class DualKeyDictionaryTests
     {
-        var dict = new DualKeyDictionary<string>();
-        dict[42] = "answer";
-        Assert.That(dict[42], Is.EqualTo("answer"));
+        [Test]
+        public void setAndGet_ByIntKey_Works_For_ReferenceType()
+        {
+            var dict = new DualKeyDictionary<string>();
+            dict[42] = "answer";
+            Assert.That(dict[42], Is.EqualTo("answer"));
 
-        dict[42] = "updated";
-        Assert.That(dict[42], Is.EqualTo("updated"));
-    }
+            dict[42] = "updated";
+            Assert.That(dict[42], Is.EqualTo("updated"));
+        }
 
-    [Test]
-    public void SetAndGet_ByStringKey_Works_For_ValueType()
-    {
-        var dict = new DualKeyDictionary<int>();
-        dict["count"] = 10;
-        Assert.That(dict["count"], Is.EqualTo(10));
+        [Test]
+        public void setAndGet_ByStringKey_Works_For_ValueType()
+        {
+            var dict = new DualKeyDictionary<int>();
+            dict["count"] = 10;
+            Assert.That(dict["count"], Is.EqualTo(10));
 
-        dict["count"] = -5;
-        Assert.That(dict["count"], Is.EqualTo(-5));
-    }
+            dict["count"] = -3;
+            Assert.That(dict["count"], Is.EqualTo(-3));
+        }
 
-    [Test]
-    public void Get_Unknown_Int_Key_Throws()
-    {
-        var dict = new DualKeyDictionary<string>();
-        Assert.Throws<KeyNotFoundException>(() => { var _ = dict[777]; });
-    }
+        [Test]
+        public void Get_Unknown_Int_Key_Throws()
+        {
+            var dict = new DualKeyDictionary<string>();
+            Assert.Throws<KeyNotFoundException>(() => { var _ = dict[777]; });
+        }
 
-    [Test]
-    public void Get_Unknown_String_Key_Throws()
-    {
-        var dict = new DualKeyDictionary<int>();
-        Assert.Throws<KeyNotFoundException>(() => { var _ = dict["nope"]; });
-    }
+        [Test]
+        public void Get_Unknown_String_Key_Throws()
+        {
+            var dict = new DualKeyDictionary<string>();
+            Assert.Throws<KeyNotFoundException>(() => { var _ = dict["nope"]; });
+        }
 
-    [Test]
-    public void String_Key_Null_Throws_On_Get_And_Set()
-    {
-        var dict = new DualKeyDictionary<string>();
-        Assert.Throws<ArgumentNullException>(() => { var _ = dict[null!]; });
-        Assert.Throws<ArgumentNullException>(() => dict[null!] = "x");
-    }
+        [Test]
+        public void String_Key_Null_Throws_On_Get_And_Set()
+        {
+            var dict = new DualKeyDictionary<string>();
+            Assert.Throws<ArgumentNullException>(() => { var _ = dict[null]; });
+            Assert.Throws<ArgumentNullException>(() => dict[null] = "w");
+        }
 
-    [Test]
-    public void Int_And_String_KeySpaces_Are_Independent()
-    {
-        var dict = new DualKeyDictionary<string>();
-        dict[1] = "one";
-        dict["1"] = "string-one";
+        [Test]
+        public void Int_And_String_KeySpaces_Are_Independent()
+        {
+            var dict = new DualKeyDictionary<string>();
+            dict[1] = "one";
+            dict["1"] = "string-one";
 
-        Assert.That(dict[1], Is.EqualTo("one"));
-        Assert.That(dict["1"], Is.EqualTo("string-one"));
+            Assert.That(dict[1], Is.EqualTo("one"));
+            Assert.That(dict["1"], Is.EqualTo("string-one"));
+        }
     }
 }
