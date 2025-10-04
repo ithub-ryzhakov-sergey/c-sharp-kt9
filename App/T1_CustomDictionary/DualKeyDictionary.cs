5146fa3da4c2ce8d2f8b6b6b31ce44d3638c3daa
user1 <user1@DESKTOP-13ENB0J>
2025-10-04 12:48:17 +0300

    1

--- App/T1_CustomDictionary/DualKeyDictionary.cs
+++ App/T1_CustomDictionary/DualKeyDictionary.cs
@@ -1,1 +1,114 @@
 namespace App.T1_CustomDictionary;
+public class DualKeyDictionary<TValue>
+{
+    private Dictionary<int, TValue> _dictByID = new Dictionary<int, TValue>();
+    private Dictionary<string, TValue> _dictByKey = new Dictionary<string, TValue>();
+
+
+
+
+
+
+
+
+
+    public TValue this[int id]
+    {
+        get
+        {
+            
+            if (!_dictByID.ContainsKey(id))
+            {
+                throw new KeyNotFoundException();
+            }
+            return _dictByID[id];
+        }
+
+
+
+
+            
+
+
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentException();
+            }
+
+            if ( _dictByID.ContainsKey(id) )
+            {
+                _dictByID[id] = value;
+            }
+
+            _dictByID[id] = value;
+        }
+    }
+
+
+
+
+
+
+
+
+    public TValue this[string key]
+    {
+        get
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (!_dictByKey.ContainsKey(key))
+            {
+                throw new KeyNotFoundException();
+            }
+            return _dictByKey[key];
+        }
+
+
+
+
+        set
+        {
+            if( value == null)
+            {
+                throw new ArgumentException();
+            }
+
+            if( _dictByKey.ContainsKey(key))
+            {
+                _dictByKey[key] = value;
+            }
+
+            _dictByKey[key] = value;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void KeyNotFoundException(string key)
+    {
+        if (!_dictByKey.ContainsKey(key))
+        {
+            throw new KeyNotFoundException();
+        }
+    }
+
+}
--- App/T2_WarehouseManager/WarehouseManager.cs
+++ App/T2_WarehouseManager/WarehouseManager.cs
@@ -1,1 +1,54 @@
 namespace App.T2_WarehouseManager;
+
+public class Product
+{
+    public int ID { get; }
+    public string Name { get; }
+
+    public Product(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+}
+
+public class Electronics : Product
+{
+    public int WarrantyMonths { get; }
+
+    public Electronics(int id, string name, int warrantyMonth)
+        : base(id, name)
+    {
+        WarrantyMonths = warrantyMonth;
+    }
+}
+
+public class Food : Product
+{
+    public DateTime ExpirationDate { get; }
+
+    public Food(int id, string name, DateTime expirationDate)
+        : base(id, name)
+    {
+        ExpirationDate = expirationDate;
+    }
+}
+
+public class WarehouseManager<T> where T : Product
+{
+    private readonly IEnumerable<T> items;
+    public WarehouseManager(IEnumerable<T> items)
+    {
+        if (items == null)
+            throw new ArgumentNullException(nameof(items));
+        this.items = items;
+    }
+
+    public void PrintBasicInfo()
+    {
+        foreach (var item in items)
+        {
+            Console.WriteLine($"ID: {item.ID}, Name: {item.Name}");
+        }
+    }
+}
