ef1e04bc8bb0978041fe534c70d0ea295e6c47a6
user1 <user1@DESKTOP-UNR49SD>
2025-10-04 14:44:58 +0300

    2 Ограниченные типы/классы (constraints)

--- App/T2_WarehouseManager/WarehouseManager.cs
+++ App/T2_WarehouseManager/WarehouseManager.cs
@@ -1,1 +1,57 @@
-namespace App.T2_WarehouseManager;
+
+
+    using System;
+    using System.Collections.Generic;
+
+    namespace App.Tests.T2_WarehouseManager
+    {
+        public abstract class Product
+        {
+            public int ID { get; protected set; }
+            public string Name { get; protected set; }
+
+            protected Product(int id, string name)
+            {
+                ID = id;
+                Name = name;
+            }
+        }
+
+        public class Electronics : Product
+        {
+            public int WarrantyMonths { get; }
+
+            public Electronics(int id, string name, int warrantyMonths) : base(id, name)
+            {
+                WarrantyMonths = warrantyMonths;
+            }
+        }
+
+        public class Food : Product
+        {
+            public DateTime ExpirationDate { get; }
+
+            public Food(int id, string name, DateTime expirationDate) : base(id, name)
+            {
+                ExpirationDate = expirationDate;
+            }
+        }
+
+        public class WarehouseManager<T> where T : Product
+        {
+            private readonly IEnumerable<T> _items;
+
+            public WarehouseManager(IEnumerable<T> items)
+            {
+                _items = items ?? throw new ArgumentNullException(nameof(items));
+            }
+
+            public void PrintBasicInfo()
+            {
+                foreach (var item in _items)
+                {
+                    Console.WriteLine($"ID: {item.ID}, Name: {item.Name}");
+                }
+            }
+        }
+    }
