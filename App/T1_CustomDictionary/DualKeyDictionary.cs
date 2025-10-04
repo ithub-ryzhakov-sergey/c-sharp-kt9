ff35199fc2eeab146f6b196c677670bae4251a40
user1 <user1@DESKTOP-UNR49SD>
2025-10-04 14:44:35 +0300

    1 Собственный словарь с двумя типами ключей

--- App/T1_CustomDictionary/DualKeyDictionary.cs
+++ App/T1_CustomDictionary/DualKeyDictionary.cs
@@ -1,1 +1,47 @@
-namespace App.T1_CustomDictionary;
+using System;
+using System.Collections.Generic;
+
+namespace App.Topics.Generics.T1_CustomDictionary
+{
+    public class DualKeyDictionary<TValue>
+    {
+        private readonly Dictionary<int, TValue> _intDictionary;
+        private readonly Dictionary<string, TValue> _stringDictionary;
+
+        public DualKeyDictionary()
+        {
+            _intDictionary = new Dictionary<int, TValue>();
+            _stringDictionary = new Dictionary<string, TValue>();
+        }
+
+        public TValue this[int id]
+        {
+            get
+            {
+                return _intDictionary[id];
+            }
+            set
+            {
+                _intDictionary[id] = value;
+            }
+        }
+
+        public TValue this[string key]
+        {
+            get
+            {
+                if (key == null)
+                    throw new ArgumentNullException(nameof(key));
+
+                return _stringDictionary[key];
+            }
+            set
+            {
+                if (key == null)
+                    throw new ArgumentNullException(nameof(key));
+
+                _stringDictionary[key] = value;
+            }
+        }
+    }
+}
